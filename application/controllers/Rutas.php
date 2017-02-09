9cdd7c6186fdc92cd1aff19a716dcd1f2815dceb
Johan Camilo Mosquera Ninco <joncamilomoni@gmail.com>
2017-02-09 18:02:44 -0500

    Personalización de Rutas con el archivo routes.php
    Codeigniter te permite personalizar las url desde el archivo routes.php
    ubicado en el directorio config
    Para utilizarla, asignar un valor a buscar al controlador que va a
    emitir la respuesta, de esta manera tenemos urls limpias como el slug en
    wordpress

---
+++ application/controllers/Rutas.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Rutas extends CI_Controller {
+
+	public function __construct(){
+		parent::__construct();
+		$this->layout->setLayout("frontend");
+	}
+
+	public function sobre_nosotros()
+	{
+		$this->layout->view("sobre_nosotros");
+	}
+	public function preguntas_frecuentes(){
+		$this->layout->view('preguntas_frecuentes');
+	}
+	public function con_parametro($id = null){
+		if (!$id) {show_404();}
+		$this->layout->view('con_parametro');
+	}
+	
+}
---
+++ application/views/rutas/preguntas_frecuentes.php
@@ -0,0 +1,5 @@
+<h2>Preguntas frecuentes</h2>
+<ul>
+	<li><?php echo $this->uri->segment(1); ?></li>
+	<li><?php echo $this->uri->segment(2); ?></li>
+</ul>

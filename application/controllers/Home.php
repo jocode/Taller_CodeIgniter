9fcb8638a4fded89dab1b9a7c7922afc9938a1dc
Johan Camilo Mosquera Ninco <joncamilomoni@gmail.com>
2017-02-09 10:23:07 -0500

    Trabajar con el Controlador Home.php
    CodeIgniter es un framework que trabaja con el patron MVC. La url
    al tener activado el módulo mod_rewrite de apache, toma la ruta de la
    siguiente manera http://servidor/controlador/metodo/parametros
    He creado un controlador llamado Home.php que hereda la clase
    CI_Controller, he creado el método constructor para que cargue también
    el método constructor de la clase padre.
    En este archivo hay tres métodos aparte del constructor, donde renderizo
    la vista, integrando la librería layout en el constructor y asignando la
    vista a su respectivo método.
    $this->layout->setLayout('name-layout') Asigno el layout que quiero
    utilizar. En el layout debo imprimir la variable global $content_for_layout
    para mostrar el contenido de la vista en ese layout.
    Los layout se deben crear dentro del directorio views->layouts. Cada
    layout debe contener la estructura del DOM e incluirle las librerías
    frontends con la que quiere trabajar

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,8 +3,27 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
+	public function __construct(){
+		parent::__construct();
+		// Definir el archivo que utilizará la librería layout
+		$this->layout->setLayout("frontend");
+	}
+	//localhost/home/index
+	# servidor/controlador/metodo
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->layout->view("index");
+	}
+	public function nosotros($id = null){
+		// Pasar datos a la vista
+		$texto = "Hola primera palabra";
+		$arreglo = array("Buenas", "Chao", "Nos vemos");
+		//Renderizar la vista
+		$this->layout->view('nosotros', compact('id', 'texto', 'arreglo'));
+	}
+
+	public function contacto(){
+		$this->layout->setLayout('template');
+		$this->layout->view('contacto');
 	}
 }

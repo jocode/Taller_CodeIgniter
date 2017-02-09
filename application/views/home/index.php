55843d1703ff3642183c51cd011860017ff65a4b
Johan Camilo Mosquera Ninco <joncamilomoni@gmail.com>
2017-02-09 18:08:13 -0500

    Trabajar con Helpers
    Codeigniter te permite crear helper que puedes usarlo desde cualquier vista.
    Para crear un helper ir al directorio helpers y crear un archivo con el
    nombrehelper_helper y luego cargarlo en el autoload en el array helper
    solo con el nombre, sin _helper porque el framework ya entiende que lo
    que est´cargando es un helper.
    Y puedes usarlo llamando el método desde cualquier vista. Es recomendle
    no imprimir nada en ls funciones y hacer la confirmacion primero si el
    método existe

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,1 +1,16 @@
 <h2>Hola. Soy la vista index, cargada desde el controlador Home, en el método index</h2>
+<?php echo saludo();?>
+<hr />
+la fecha de hoy es : <?php echo fecha("2016-09-20");?>
+<hr />
+Tu edad es  : <?php echo calculaEdad("1980-05-24");?>
+<hr />
+<?php 
+if(detectar_SO()==false)
+{
+    echo "es un PC";
+}else
+{
+    echo "es un movil";
+}
+?>

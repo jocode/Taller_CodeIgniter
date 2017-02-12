e25b2ec785d05e504932562482076c692bb06eb8
Johan Camilo Mosquera Ninco <joncamilomoni@gmail.com>
2017-02-11 21:30:29 -0500

    Nuevo script de base de datos
    He agregado el nuevo script con la base de datos, que incluye la tabla
    productos_fotos
    
    Para más información sobre carga de archivos, revisar la documentación
    https://codeigniter.com/user_guide/libraries/file_uploading.html?highlight=uploading

--- script_db/taller_codeigniter.sql
+++ script_db/taller_codeigniter.sql
@@ -1,68 +1,43 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 10-02-2017 a las 22:07:17
--- Versión del servidor: 10.1.16-MariaDB
--- Versión de PHP: 5.6.24
+﻿# Host: localhost  (Version 5.5.5-10.1.16-MariaDB)
+# Date: 2017-02-11 21:28:39
+# Generator: MySQL-Front 6.0  (Build 1.21)
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+/*!40101 SET NAMES utf8 */;
 
+#
+# Structure for table "productos"
+#
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `taller_codeigniter`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `productos`
---
-
+DROP TABLE IF EXISTS `productos`;
 CREATE TABLE `productos` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
   `precio` decimal(10,0) NOT NULL,
   `stock` int(11) NOT NULL,
-  `fecha` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+  `fecha` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
---
--- Volcado de datos para la tabla `productos`
---
+#
+# Data for table "productos"
+#
 
-INSERT INTO `productos` (`id`, `nombre`, `precio`, `stock`, `fecha`) VALUES
-(1, 'Mesa de computador', '120000', 6, '2017-02-10'),
-(2, 'Ampolleta electrónica', '14563', 50, '2017-02-10'),
-(3, 'Priducto ñandú', '1548', 20, '2017-02-09'),
-(4, 'Célula electrica', '100000', 5, '2016-11-08');
+INSERT INTO `productos` VALUES (1,'Mesa de computador',120000,6,'2017-02-10'),(2,'Ampolleta electrónica',14563,50,'2017-02-10'),(3,'Producto ñandú',1548,20,'2017-02-09'),(4,'Célula electrica',100000,5,'2016-11-08'),(5,'Bolso',109383,12,'2017-02-12');
 
---
--- Índices para tablas volcadas
---
+#
+# Structure for table "productos_fotos"
+#
 
---
--- Indices de la tabla `productos`
---
-ALTER TABLE `productos`
-  ADD PRIMARY KEY (`id`);
+DROP TABLE IF EXISTS `productos_fotos`;
+CREATE TABLE `productos_fotos` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_producto` int(11) DEFAULT NULL,
+  `foto` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
+#
+# Data for table "productos_fotos"
+#
 
---
--- AUTO_INCREMENT de la tabla `productos`
---
-ALTER TABLE `productos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+INSERT INTO `productos_fotos` VALUES (1,1,'50a0fdf58e71f68c3ec3ed245558ac64.JPG'),(2,4,'0334e790909c8819fa3e3a175824aa9f.jpg'),(3,3,'eff598d593beaa1538031a84e97ff55d.JPG'),(5,4,'ec1536bcac715bcba92fc093a806192f.png'),(6,5,'b757559d200c93a9454698f96bd4e35d.png');
